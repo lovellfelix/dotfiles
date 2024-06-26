0bf352c92071cbab4d7eb79158907fda5c261149
Lovell Felix <1061067+lovellfelix@users.noreply.github.com>
2024-06-26 16:51:30 -0500

    chore: Add k9s configuration files and installation script

---
+++ k9s/install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -ex
+
+# create k9s folders, if not exist
+mkdir -p "${XDG_CONFIG_HOME}"/k9s
+mkdir -p "${XDG_CONFIG_HOME}"/k9s/skins
+
+# create config symlink
+ln -sfv "${HOME}"/.dotfiles/k9s/config.yaml "${XDG_CONFIG_HOME}"/k9s/config.yaml
+ln -sfv "${HOME}"/.dotfiles/k9s/aliases.yaml "${XDG_CONFIG_HOME}"/k9s/aliases.yaml
+
+# create skin symlink
+ln -sfv "${HOME}"/.dotfiles/k9s/skins/catppuccin-mocha.yaml "${XDG_CONFIG_HOME}"/k9s/skins/catppuccin-mocha.yaml
