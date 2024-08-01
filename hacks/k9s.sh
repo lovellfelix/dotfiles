9513d65b2b8ad98f4866c159b850c80b0b21b44f
Lovell Felix <1061067+lovellfelix@users.noreply.github.com>
2024-08-01 07:56:08 -0500

    chore: Update k9s configuration to use Catppuccino Macchiato color scheme and add custom skins

--- hacks/k9s.sh
+++ hacks/k9s.sh
@@ -6,9 +6,11 @@ mkdir -p "${XDG_CONFIG_HOME}"/k9s
 mkdir -p "${XDG_CONFIG_HOME}"/k9s/skins
 
 # create config symlink
-ln -sfv "${HOME}"/.dotfiles/k9s/config.yaml "${XDG_CONFIG_HOME}"/k9s/config.yaml
-ln -sfv "${HOME}"/.dotfiles/k9s/aliases.yaml "${XDG_CONFIG_HOME}"/k9s/aliases.yaml
+ln -sfv "${HOME}"/.dotfiles/k9s/.config/k9s/config.yaml "${XDG_CONFIG_HOME}"/k9s/config.yaml
+ln -sfv "${HOME}"/.dotfiles/k9s/.config/k9s/aliases.yaml "${XDG_CONFIG_HOME}"/k9s/aliases.yaml
 
 # create skin symlink
-ln -sfv "${HOME}"/.dotfiles/k9s/skins/catppuccin-mocha.yaml "${XDG_CONFIG_HOME}"/k9s/skins/catppuccin-mocha.yaml
-ln -sfv "${HOME}"/.dotfiles/k9s/skins/tokyo-night.yaml "${XDG_CONFIG_HOME}"/k9s/skins/tokyo-night.yaml
+ln -sfv "${HOME}"/.dotfiles/k9s/.config/k9s/skins/catppuccin-macchiato.yaml "${XDG_CONFIG_HOME}"/k9s/skins/catppuccin-macchiato.yaml
+ln -sfv "${HOME}"/.dotfiles/k9s/.config/k9s/skins/catppuccin-macchiato-transparent.yaml "${XDG_CONFIG_HOME}"/k9s/skins/catppuccin-macchiato-transparent.yaml
+ln -sfv "${HOME}"/.dotfiles/k9s/.config/k9s/skins/catppuccin-custom.yaml "${XDG_CONFIG_HOME}"/k9s/skins/catppuccin-custom.yaml
+
