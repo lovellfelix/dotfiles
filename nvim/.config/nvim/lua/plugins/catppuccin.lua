7a1af1832293e0d4f2b4c813c76d9b20b1f2db66
Lovell Felix <1061067+lovellfelix@users.noreply.github.com>
2024-08-19 07:47:56 -0500

    chore: Add Catppuccino and Neo Tree plugins to nvim configuration

---
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,12 @@
+return {
+  { "catppuccin/nvim" },
+
+  -- Configure LazyVim to load catppuccin macchiato colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-macchiato",
+    },
+  },
+
+}
